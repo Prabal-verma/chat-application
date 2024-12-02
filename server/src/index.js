c100c031e2bcff3e21ea0a4cda8389799dfb8e10
Prabal Verma <prableverma@gmail.com>
2024-12-03 03:18:54 +0530

    user model

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"    
+import authRoutes from "./routes/auth.route.js"  
+import dotenv from "dotenv"  
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5001,()=>{
-  console.log("server started at port 5001");
+app.listen(PORT,()=>{
+  console.log(`server started at port ${PORT}`);
+  connectDB();
 })
