3d1fdb9852f38d9003d619e987c4562e7aad3439
Prabal Verma <prableverma@gmail.com>
2024-12-03 04:10:28 +0530

    middleware

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { checkAuth ,login, logout, signup ,updateProfile  } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -10,5 +12,6 @@ router.post("/logout",logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
