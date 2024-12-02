c4671b2df66e9c11cab84f529f8b677530e95bbb
Prabal Verma <prableverma@gmail.com>
2024-12-03 04:07:26 +0530

    Authentication

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -2,10 +2,13 @@ import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
-router.get("/login", login);
+router.post("/login", login);
 
-router.get("/logout",logout);
+router.post("/logout",logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
 
 export default router;
