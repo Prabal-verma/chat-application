efa394304f189bbbd8c45fcaebe7c98ec5e8a976
Prabal Verma <prableverma@gmail.com>
2024-12-03 04:32:35 +0530

    cookie parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"  
 import dotenv from "dotenv"  
 import { connectDB } from "./lib/db.js";
@@ -7,6 +8,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
